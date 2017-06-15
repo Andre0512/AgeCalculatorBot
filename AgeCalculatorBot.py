f7a728eb2032c3ea6e7d80acf9b1c79fb3cce864
Andre <andre.basche@gmail.com>
2017-06-15 21:50:00 +0200

    Added insert start date

--- AgeCalculatorBot.py
+++ AgeCalculatorBot.py
@@ -19,6 +19,20 @@ def get_yml(file):
     return result
 
 
+def get_year_kb(c, r, year):
+    row = []
+    std_year = year
+    for i in range(r):
+        column = []
+        for j in range(c):
+            column.append(InlineKeyboardButton(str(year), callback_data="year" + ' ' + str(year)))
+            year = 1 + year
+        row.append(column)
+    row.append([InlineKeyboardButton("Previous", callback_data="prev" + ' ' + str(std_year)),
+                InlineKeyboardButton("Next", callback_data="next" + ' ' + str(std_year))])
+    return InlineKeyboardMarkup(row)
+
+
 def get_number_kb(c, r, callback, limit=99, name_list=()):
     row = []
     for i in range(r):
@@ -37,18 +51,37 @@ def start(bot, update):
     keyboard = get_number_kb(8, 4, "day", limit=31)
     month = ('Jan', 'Feb', 'Mrz', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez')
     # month = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
-    #keyboard = get_number_kb(3, 4, "month", name_list=month)
+    # keyboard = get_number_kb(3, 4, "month", name_list=month)
     update.message.reply_text('Startdatum: xx.xx.xxxx', reply_markup=keyboard)
+
+
+def navigate_year(update, chat_data, direction):
+    new_year = int(update.callback_query.data.split(" ")[1])
+    new_year = new_year + 12 if direction == "next" else new_year - 12
+    reply_text = 'Startdatum: ' + str(chat_data["day"]) + "." + str(chat_data["month"]) + '.xxxx'
+    update.callback_query.message.edit_text(reply_text, reply_markup=get_year_kb(4, 3, new_year))
+
 
 def button(bot, update, chat_data):
     update.callback_query.answer()
-    if update.callback_query.data.split(" ")[0] == "day":
+    arg_one = update.callback_query.data.split(" ")[0]
+    if arg_one == "day":
         chat_data["day"] = update.callback_query.data.split(" ")[1]
         month = ('Jan', 'Feb', 'Mrz', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez')
         reply_text = 'Startdatum: ' + str(chat_data["day"]) + '.xx.xxxx'
         update.callback_query.message.edit_text(reply_text, reply_markup=get_number_kb(3, 4, "month", name_list=month))
-    elif update.callback_query.data.split(" ")[0] == "month":
-        chat_data["day"] = update.callback_query.data.split(" ")[1]
+    elif arg_one == "month":
+        chat_data["month"] = update.callback_query.data.split(" ")[1]
+        reply_text = 'Startdatum: ' + str(chat_data["day"]) + "." + str(chat_data["month"]) + '.xxxx'
+        update.callback_query.message.edit_text(reply_text, reply_markup=get_year_kb(4, 3, 1980))
+    elif arg_one == "year":
+        chat_data["year"] = update.callback_query.data.split(" ")[1]
+        reply_text = 'Startdatum: ' + str(chat_data["day"]) + "." + str(chat_data["month"]) + '.' + str(
+            chat_data["year"])
+        update.callback_query.message.edit_text(reply_text)
+    elif arg_one == "next" or arg_one == "prev":
+        navigate_year(update, chat_data, arg_one)
+
 
 def main():
     updater = Updater(get_yml('./config.yml')['agecalculator']['bottoken'])
