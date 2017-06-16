4979063d3f997f1dc58b830eaab9c5d5174f51f9
Andre <andre.basche@gmail.com>
2017-06-16 15:34:29 +0200

    Languages english and german

--- AgeCalculatorBot.py
+++ AgeCalculatorBot.py
@@ -3,12 +3,16 @@
 
 import os
 import logging
+import yaml
 from dateutil.relativedelta import relativedelta
 from _datetime import datetime
-import yaml
-import time
-from telegram import ReplyKeyboardMarkup, KeyboardButton, ParseMode, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import ParseMode, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+
+
+def custom_str_constructor(loader, node):
+    return loader.construct_scalar(node).encode('utf-8')
+
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -17,12 +21,29 @@ logger = logging.getLogger(__name__)
 
 
 def get_yml(file):
+    result = {}
     with open(os.path.join(os.path.dirname(__file__), file), 'rb') as ymlfile:
-        result = yaml.load(ymlfile)
+        values = yaml.load(ymlfile)
+        for k, v in values.items():
+            result[k.decode('utf-8')] = dict_byte_to_str(v)
     return result
 
 
-def get_year_kb(c, r, year, arg):
+def dict_byte_to_str(v):
+    result = {}
+    if hasattr(v, 'items'):
+        for key, value in v.items():
+            if isinstance(value, bytes):
+                value = value.decode('utf-8')
+                value = str.replace(value, "\\n", "\n")
+            result[key.decode('utf-8')] = value
+    else:
+        result = v.decode('utf-8')
+        result = str.replace(result, "\\n", "\n")
+    return result
+
+
+def get_year_kb(c, r, year, arg, chat_data):
     row = []
     std_year = year
     for i in range(r):
@@ -31,8 +52,10 @@ def get_year_kb(c, r, year, arg):
             column.append(InlineKeyboardButton(str(year), callback_data=arg + ' ' + str(year)))
             year = 1 + year
         row.append(column)
-    row.append([InlineKeyboardButton("⬅️ Früher", callback_data=arg[0:1] + "prev" + ' ' + str(std_year)),
-                InlineKeyboardButton("Später ➡️", callback_data=arg[0:1] + "next" + ' ' + str(std_year))])
+    row.append([InlineKeyboardButton("⬅️ " + strings[chat_data["lang"]]["early"],
+                                     callback_data=arg[0:1] + "prev" + ' ' + str(std_year)),
+                InlineKeyboardButton(strings[chat_data["lang"]]["late"] + " ➡️",
+                                     callback_data=arg[0:1] + "next" + ' ' + str(std_year))])
     return InlineKeyboardMarkup(row)
 
 
@@ -50,14 +73,32 @@ def get_number_kb(c, r, callback, limit=99, name_list=()):
     return InlineKeyboardMarkup(row)
 
 
+def get_language(user):
+    try:
+        language = user.language_code
+        language = language.split('-')[0]
+    except:
+        language = 'en'
+    return language
+
+
 def start(bot, update, chat_data):
+    user = update.callback_query.from_user if update.callback_query else update.message.from_user
+    if not "lang" in chat_data:
+        chat_data["lang"] = get_language(user)
+    name = user.first_name if update.callback_query else user.first_name
+    reply = update.callback_query.message.edit_text if update.callback_query else update.message.reply_text
+    text = strings[chat_data["lang"]]["ok"] + " 😁" if "confirmed" in chat_data else strings[chat_data["lang"]][
+        "question"]
+    reply(strings[chat_data["lang"]]["greeting"] + " " + name
+          + " ✌🏻\n" + text, reply_markup=get_lang_keyboard(chat_data))
+
+
+def send(bot, update, chat_data):
     keyboard = get_number_kb(8, 4, "sday", limit=31)
-    month = ('Jan', 'Feb', 'Mrz', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez')
-    # month = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
-    # keyboard = get_number_kb(3, 4, "month", name_list=month)
     delete_date(chat_data, 's')
     delete_date(chat_data, 'g')
-    update.message.reply_text(get_text(chat_data) + get_action("sday"),
+    update.message.reply_text(get_text(chat_data) + get_action("sday", chat_data),
                               reply_markup=keyboard,
                               parse_mode=ParseMode.MARKDOWN)
 
@@ -65,56 +106,77 @@ def start(bot, update, chat_data):
 def navigate_year(update, chat_data, arg):
     new_year = int(update.callback_query.data.split(" ")[1])
     new_year = new_year + 12 if arg == "gnext" or arg == "snext" else new_year - 12
-    update.callback_query.message.edit_text(get_text(chat_data) + get_action(arg[0:1] + "year"),
-                                            reply_markup=get_year_kb(4, 3, new_year, arg[0:1] + "year"),
+    update.callback_query.message.edit_text(get_text(chat_data) + get_action(arg[0:1] + "year", chat_data),
+                                            reply_markup=get_year_kb(4, 3, new_year, arg[0:1] + "year", chat_data),
                                             parse_mode=ParseMode.MARKDOWN)
 
 
 def get_text(chat_data):
-    result = "Geburtstag: *"
+    result = strings[chat_data["lang"]]["birthday"] + ": *"
     result = result + str(chat_data["sday"]) + "." if "sday" in chat_data else result + "xx."
     result = result + str(chat_data["smonth"]) + "." if "smonth" in chat_data else result + "xx."
     result = result + str(chat_data["syear"]) + "*" if "syear" in chat_data else result + "xxxx*"
-    result = result + "\nHeutiges Datum: *"
+    result = result + "\n" + strings[chat_data["lang"]]["todays_date"] + ": *"
     result = result + str(chat_data["gday"]) + "." if "gday" in chat_data else result + "xx."
     result = result + str(chat_data["gmonth"]) + "." if "gmonth" in chat_data else result + "xx."
     result = result + str(chat_data["gyear"]) + "*" if "gyear" in chat_data else result + "xxxx*"
     return result
 
 
-def get_goal_keyboard():
-    keyboard = [[InlineKeyboardButton("📅 " + "Heute", callback_data="today")],
-                [InlineKeyboardButton("✏️ " + "Anderes Datum ", callback_data="insert")]]
+def get_goal_keyboard(chat_data):
+    keyboard = [[InlineKeyboardButton("📅 " + strings[chat_data["lang"]]["today"], callback_data="today")],
+                [InlineKeyboardButton("✏️ " + strings[chat_data["lang"]]["other_date"], callback_data="insert")]]
     return InlineKeyboardMarkup(keyboard)
 
 
-def get_result_keyboard(exclude):
-    keyboard = [[InlineKeyboardButton("⏳ " + "Alter", callback_data="calc")],
-                [InlineKeyboardButton("📊 " + "Absolutes Alter", callback_data="total")],
-                [InlineKeyboardButton("📆 " + "Nächste Geburtstage", callback_data="next_birthdays")]]
+def get_lang_keyboard(chat_data):
+    if "confirmed" in chat_data:
+        chat_data.pop("confirmed", None)
+        return None
+    else:
+        if chat_data["lang"] == "de":
+            emoji = "🇬🇧"
+            callback = "en"
+        else:
+            emoji = "🇩🇪"
+            callback = "de"
+        keyboard = [[InlineKeyboardButton("✔️ " + strings[chat_data["lang"]]["ja"], callback_data="yes")],
+                    [InlineKeyboardButton(
+                        strings[chat_data["lang"]]["nein"] + ", " + strings[chat_data["lang"]][
+                            "other_lang"] + " " + emoji,
+                        callback_data=callback)]]
+        return InlineKeyboardMarkup(keyboard)
+
+
+def get_result_keyboard(exclude, chat_data):
+    keyboard = [[InlineKeyboardButton("⏳ " + strings[chat_data["lang"]]["age"], callback_data="calc")],
+                [InlineKeyboardButton("📊 " + strings[chat_data["lang"]]["total"], callback_data="total")],
+                [InlineKeyboardButton("📆 " + strings[chat_data["lang"]]["next"], callback_data="next_birthdays")]]
     keyboard.pop(exclude)
     return InlineKeyboardMarkup(keyboard)
 
 
-def get_calc_keyboard():
-    keyboard = [[InlineKeyboardButton("🎛 " + "Berechnen", callback_data="calc")],
-                [InlineKeyboardButton("✏️ " + "Korrigiere Geburtstag", callback_data="correct_start")],
-                [InlineKeyboardButton("✏️ " + "Korrigiere heutiges Datum", callback_data="correct_goal")]]
+def get_calc_keyboard(chat_data):
+    keyboard = [[InlineKeyboardButton("🎛 " + strings[chat_data["lang"]]["calc"], callback_data="calc")],
+                [InlineKeyboardButton("✏️ " + strings[chat_data["lang"]]["correct_bday"],
+                                      callback_data="correct_start")],
+                [InlineKeyboardButton("✏️ " + strings[chat_data["lang"]]["correct_today"],
+                                      callback_data="correct_goal")]]
     return InlineKeyboardMarkup(keyboard)
 
 
-def get_action(arg):
+def get_action(arg, chat_data):
     if arg[1:] == "day":
-        period = "den Tag"
+        period = strings[chat_data["lang"]]["t_day"]
     elif arg[1:] == "month":
-        period = "den Monat"
+        period = strings[chat_data["lang"]]["t_month"]
     else:
-        period = "das Jahr"
+        period = strings[chat_data["lang"]]["t_year"]
     if arg[0:1] == "s":
-        day = "deines Geburtstags"
+        day = strings[chat_data["lang"]]["t_birthday"]
     else:
-        day = "des heutigen Tages"
-    return "\n\n💡 " + "Bitte gebe " + period + " " + day + " ein:"
+        day = strings[chat_data["lang"]]["t_todays_date"]
+    return "\n\n💡 " + strings[chat_data["lang"]]["action_start"] + " " + period + " " + day + " " + ":"
 
 
 def delete_date(chat_data, arg):
@@ -126,9 +188,9 @@ def delete_date(chat_data, arg):
 def time_since(chat_data):
     d1 = datetime.strptime(chat_data["sday"] + "." + chat_data["smonth"] + "." + chat_data["syear"], "%d.%m.%Y")
     d2 = datetime.strptime(chat_data["gday"] + "." + chat_data["gmonth"] + "." + chat_data["gyear"], "%d.%m.%Y")
-    result = "*" + str(relativedelta(d2, d1).years) + "* Jahre\n"
-    result = result + "*" + str(relativedelta(d2, d1).months) + "* Monate\n"
-    result = result + "*" + str(relativedelta(d2, d1).days) + "* Tage\n"
+    result = "*" + str(relativedelta(d2, d1).years) + "* " + strings[chat_data["lang"]]["years"] + "\n"
+    result = result + "*" + str(relativedelta(d2, d1).months) + "* " + strings[chat_data["lang"]]["months"] + "\n"
+    result = result + "*" + str(relativedelta(d2, d1).days) + "* " + strings[chat_data["lang"]]["days"] + "\n"
     return result
 
 
@@ -138,8 +200,8 @@ def time_to(chat_data):
     if d1 < d2:
         d1 = datetime.strptime(chat_data["sday"] + "." + chat_data["smonth"] + "." + str((int(chat_data["gyear"]) + 1)),
                                "%d.%m.%Y")
-    result = "*" + str(relativedelta(d1, d2).months) + " *Monate\n"
-    result = result + "*" + str(relativedelta(d1, d2).days) + "* Tage\n"
+    result = "*" + str(relativedelta(d1, d2).months) + "* " + strings[chat_data["lang"]]["months"] + "\n"
+    result = result + "*" + str(relativedelta(d1, d2).days) + "* " + strings[chat_data["lang"]]["days"] + "\n"
     return result
 
 
@@ -150,18 +212,19 @@ def total_time(chat_data):
     days = diff.days
     seconds = diff.seconds
 
-    result = "*" + str(int(days / 365.25)) + "* Jahre\n"
-    result = result + "*" + str(int(days / 30.4375)) + "* Monate\n"
-    result = result + "*" + str(int(days)) + "* Tage\n"
-    result = result + "*" + str(int(days * 24 + seconds / 3600)) + "* Stunden\n"
-    result = result + "*" + str(int(days * 24 * 60 + seconds / 60)) + "* Minuten\n"
-    result = result + "*" + str(int(days * 24 * 3600 + seconds)) + "* Sekunden\n"
+    result = "*" + str(int(days / 365.25)) + "* " + strings[chat_data["lang"]]["years"] + "\n"
+    result = result + "*" + str(int(days / 30.4375)) + "* " + strings[chat_data["lang"]]["months"] + "\n"
+    result = result + "*" + str(int(days)) + "* " + strings[chat_data["lang"]]["days"] + "\n"
+    result = result + "*" + str(int(days * 24 + seconds / 3600)) + "* " + strings[chat_data["lang"]]["hours"] + "\n"
+    result = result + "*" + str(int(days * 24 * 60 + seconds / 60)) + "* " + strings[chat_data["lang"]][
+        "minutes"] + "\n"
+    result = result + "*" + str(int(days * 24 * 3600 + seconds)) + "* " + strings[chat_data["lang"]]["seconds"] + "\n"
     return result
 
 
 def weekdays(chat_data):
-    days = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"]
-    result = "Wochentage:\n"
+    days = strings[chat_data["lang"]]["days_list"].split(", ")
+    result = strings[chat_data["lang"]]["weekdays"] + ":\n"
     for i in range(10):
         result = result + chat_data["sday"] + "." + chat_data["smonth"] + "." + str(
             int(chat_data["gyear"]) + i) + ": *" + days[int(
@@ -175,19 +238,20 @@ def button(bot, update, chat_data):
     arg_one = update.callback_query.data.split(" ")[0]
     if arg_one == "sday" or arg_one == "gday":
         chat_data[arg_one] = update.callback_query.data.split(" ")[1]
-        month = ('Jan', 'Feb', 'Mrz', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez')
-        update.callback_query.message.edit_text(get_text(chat_data) + get_action(arg_one[0:1] + "month"),
+        month = strings[chat_data["lang"]]["month_list"].split(", ")
+        update.callback_query.message.edit_text(get_text(chat_data) + get_action(arg_one[0:1] + "month", chat_data),
                                                 reply_markup=get_number_kb(3, 4, arg_one[0:1] + "month",
                                                                            name_list=month),
                                                 parse_mode=ParseMode.MARKDOWN)
     elif arg_one == "smonth" or arg_one == "gmonth":
         chat_data[arg_one] = update.callback_query.data.split(" ")[1]
-        update.callback_query.message.edit_text(get_text(chat_data) + get_action(arg_one[0:1] + "year"),
-                                                reply_markup=get_year_kb(4, 3, 1980, arg_one[0:1] + "year"),
+        update.callback_query.message.edit_text(get_text(chat_data) + get_action(arg_one[0:1] + "year", chat_data),
+                                                reply_markup=get_year_kb(4, 3, 1980, arg_one[0:1] + "year", chat_data),
                                                 parse_mode=ParseMode.MARKDOWN)
     elif arg_one == "syear" or arg_one == "gyear":
         chat_data[arg_one] = update.callback_query.data.split(" ")[1]
-        keyboard = get_calc_keyboard() if "syear" in chat_data and "gyear" in chat_data else get_goal_keyboard()
+        keyboard = get_calc_keyboard(chat_data) if "syear" in chat_data and "gyear" in chat_data else get_goal_keyboard(
+            chat_data)
         update.callback_query.message.edit_text(get_text(chat_data), reply_markup=keyboard,
                                                 parse_mode=ParseMode.MARKDOWN)
     elif arg_one == "snext" or arg_one == "sprev" or arg_one == "gnext" or arg_one == "gprev":
@@ -197,44 +261,59 @@ def button(bot, update, chat_data):
         chat_data["gmonth"] = datetime.now().strftime("%m")
         chat_data["gyear"] = datetime.now().strftime("%Y")
         update.callback_query.message.edit_text(get_text(chat_data), parse_mode=ParseMode.MARKDOWN,
-                                                reply_markup=get_calc_keyboard())
+                                                reply_markup=get_calc_keyboard(chat_data))
     elif arg_one == "insert":
         keyboard = get_number_kb(8, 4, "gday", limit=31)
-        update.callback_query.message.edit_text(get_text(chat_data) + get_action("gday"),
+        update.callback_query.message.edit_text(get_text(chat_data) + get_action("gday", chat_data),
                                                 reply_markup=keyboard,
                                                 parse_mode=ParseMode.MARKDOWN)
     elif arg_one == "correct_start" or arg_one == "correct_goal":
         delete_date(chat_data, arg_one.split("correct_")[1][0:1])
         keyboard = get_number_kb(8, 4, arg_one.split("correct_")[1][0:1] + "day", limit=31)
         update.callback_query.message.edit_text(
-            get_text(chat_data) + get_action(arg_one.split("correct_")[1][0:1] + "day"),
+            get_text(chat_data) + get_action(arg_one.split("correct_")[1][0:1] + "day", chat_data),
             reply_markup=keyboard,
             parse_mode=ParseMode.MARKDOWN)
     elif arg_one == "calc":
         try:
-            text = "Alter:\n" + time_since(chat_data) + "\nNächster Geburtstag:\n" + time_to(chat_data)
+            text = strings[chat_data["lang"]]["age"] + ":\n" + time_since(chat_data) + "\n" + \
+                   strings[chat_data["lang"]]["next"] + ":\n" + time_to(chat_data)
             update.callback_query.message.edit_text(get_text(chat_data) + "\n\n" + text, parse_mode=ParseMode.MARKDOWN,
-                                                    reply_markup=get_result_keyboard(0))
+                                                    reply_markup=get_result_keyboard(0, chat_data))
         except ValueError:
-            update.callback_query.message.reply_text("Achtung, ungültiges Datum...\nVersuche es bitte erneut")
+            update.callback_query.message.reply_text(strings[chat_data["lang"]]["error"])
             start(bot, update.callback_query, chat_data)
     elif arg_one == "total":
-        text = "Total:\n" + total_time(chat_data)
+        text = strings[chat_data["lang"]]["total"] + ":\n" + total_time(chat_data)
         update.callback_query.message.edit_text(get_text(chat_data) + "\n\n" + text, parse_mode=ParseMode.MARKDOWN,
-                                                reply_markup=get_result_keyboard(1))
+                                                reply_markup=get_result_keyboard(1, chat_data))
     elif arg_one == "next_birthdays":
         text = weekdays(chat_data)
         update.callback_query.message.edit_text(get_text(chat_data) + "\n\n" + text, parse_mode=ParseMode.MARKDOWN,
-                                                reply_markup=get_result_keyboard(2))
+                                                reply_markup=get_result_keyboard(2, chat_data))
+    elif arg_one == "yes":
+        chat_data["confirmed"] = "yes"
+        start(bot, update, chat_data)
+        send(bot, update.callback_query, chat_data)
+    elif arg_one in strings:
+        chat_data["confirmed"] = "yes"
+        chat_data["lang"] = arg_one
+        start(bot, update, chat_data)
+        send(bot, update.callback_query, chat_data)
 
 
 def main():
+    yaml.add_constructor(u'tag:yaml.org,2002:str', custom_str_constructor)
+
+    global strings
+    strings = get_yml("./strings.yml")
+
     updater = Updater(get_yml('./config.yml')['agecalculator']['bottoken'])
 
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start, pass_chat_data=True))
-    dp.add_handler(MessageHandler(Filters.text, start, pass_chat_data=True))
+    dp.add_handler(MessageHandler(Filters.text, send, pass_chat_data=True))
     dp.add_handler(CallbackQueryHandler(button, pass_chat_data=True))
 
     updater.start_polling()
